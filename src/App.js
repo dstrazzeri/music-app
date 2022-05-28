d1d9d70ae4e15e339e11b2acd9b503ccb96704b4
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-05-28 04:47:42 -0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         return currentSongIndex + 1;
       }
     });
-  }, [currentSongIndex]);
+  }, [currentSongIndex, songs.length]);
 
   return (
     <div className="App">
